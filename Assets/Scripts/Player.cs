f3ccd99df7fa301ea39971a956b324492a037272
PhantomDSYT <52130475+PhantomDSYT@users.noreply.github.com>
2021-02-11 21:15:47 -0500

    End of BASIC TUTORIAL
    End of BASIC TUTORIAL by Imphenzia, made a little change by making the super jumps different from coins

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,73 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private Transform groundCheckTransform = null;
+    [SerializeField] private LayerMask playerMask;
+    private bool jumpKeyWasPressed;
+    private float horizontalInput;
+    private Rigidbody rigidbodyComponent;
+    private bool superJumpKeyWasPressed;
+    private int coins = 0;
+    private int superJumpsRemaining = 0;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbodyComponent = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpKeyWasPressed = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            superJumpKeyWasPressed = true;
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbodyComponent.velocity = new Vector3(horizontalInput, rigidbodyComponent.velocity.y, 0);
+
+        if (superJumpKeyWasPressed && superJumpsRemaining > 0)
+        {
+            rigidbodyComponent.AddForce(Vector3.up * 12, ForceMode.VelocityChange);
+            superJumpKeyWasPressed = false;
+            superJumpsRemaining--;
+        }
         
+        if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f, playerMask).Length == 0)
+        {
+            return;
+        }
+
+        if (jumpKeyWasPressed)
+        {
+            rigidbodyComponent.AddForce(Vector3.up * 7, ForceMode.VelocityChange);
+            jumpKeyWasPressed = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == 9)
+        {
+            coins++;
+            Destroy(other.gameObject);
+        }
+
+        if(other.gameObject.layer == 10)
+        {
+            superJumpsRemaining++;
+            Destroy(other.gameObject);
+        }
     }
 }
